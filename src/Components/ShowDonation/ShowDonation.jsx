f3ab9cc7817cde45d6756c63598663790bfb82e4
RashedaAkther <rashedaakther006@gmail.com>
2023-09-25 20:29:44 +0600

    showDonation cards are showed by useparams

--- src/Components/ShowDonation/ShowDonation.jsx
+++ src/Components/ShowDonation/ShowDonation.jsx
@@ -3,15 +3,40 @@ import { useLoaderData, useParams } from "react-router-dom";
 const ShowDonation = () => {
   const load = useLoaderData();
 
-  const { id } = useParams();
-  const findDonation = load.find(donation => donation.id === id)
-  console.log(findDonation);
+    const params = useParams();
+    console.log(params);
+  const findDonation = load.find(
+    (donation) => donation.id === parseInt(params.id)
+    );
+    const  {id,picture,title,category,card_bg_color,text_color,button_bg_color} = findDonation
+ 
 
     return (
-        <div>
-      
+      <div>
+        <div
+          style={{ background: card_bg_color }}
+          className="card h-96 w-full shadow-xl"
+        >
+          <figure className="px-2 pt-2">
+            <img src={picture} alt="" />
+          </figure>
+          <div className="card-actions my-3 mx-4">
+            <button
+              style={{ background: button_bg_color, color: text_color }}
+              className="btn"
+            >
+              {category}
+            </button>
+          </div>
+          <div className="card-body">
+            <h2 style={{ color: text_color }} className="card-title">
+              {title}
+            </h2>
+            <p></p>
+          </div>
         </div>
-    )
+      </div>
+    );
 };
 
 export default ShowDonation;
